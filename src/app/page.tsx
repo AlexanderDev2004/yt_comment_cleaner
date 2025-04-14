a157d06483f69026afb063bc5c4f1518ab067f5f
AlexanderDev2004 <alexgaming2120@gmail.com>
2025-04-14 08:41:16 +0700

    fix: improve text clarity and formatting in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import { FaGithub } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
 
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -11,14 +10,26 @@ export default function Home() {
           <span className="text-[#FF0000]">YT</span> Comment Cleaner
         </h1>
         <p className="text-[16px] text-center sm:text-left">
-          Clean your YouTube comments with this tool 
+          Clean up your YouTube comments with this tool from annoying comments.
+          This tool helps you remove unwanted comments from your YouTube videos
+          such as gambling comments.{" "}
         </p>
         <div className="flex flex-col gap-[16px] sm:flex-row">
-          <a  href="https://github.com/AlexanderDev2004" target="_blank" rel="noopener noreferrer" className="flex items-center gap-[8px] text-[16px] font-semibold text-[#FF0000]">
+          <a
+            href="https://github.com/AlexanderDev2004"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-[8px] text-[16px] font-semibold text-[#FF0000]"
+          >
             {" "}
-           <FaGithub />
+            <FaGithub />
           </a>
-          <a  href="https://www.instagram.com/alexjynx01/" target="_blank" rel="noopener noreferrer" className="flex items-center gap-[8px] text-[16px] font-semibold text-[#FF0000]">
+          <a
+            href="https://www.instagram.com/alexjynx01/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-[8px] text-[16px] font-semibold text-[#FF0000]"
+          >
             {" "}
             <FaInstagram />
           </a>
